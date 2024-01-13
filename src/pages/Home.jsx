b520912cd42be92a8de446c28473084ec248ea83
Faizaan Anche <faizaananche2002@gmail.com>
2024-01-13 16:19:13 +0530

    added search bar, search for movies on clicking search button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
+
 const Home = () => {
-  return <div>Home</div>;
+  const [searchStr, setSearchString] = useState('');
+
+  const onSearchInputChange = e => {
+    setSearchString(e.target.value);
+  };
+
+  const onSearch = async e => {
+    e.preventDefault();
+
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    );
+    const body = response.json();
+    console.log(body);
+
+    // console.log(e.target[0].value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input
+          type="text"
+          value={searchStr}
+          in
+          onChange={onSearchInputChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default Home;
